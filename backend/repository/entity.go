5c1fa3aa098525850dfbbd2425bae28fa928df87
Aris Firmansyah <arisfirmansyh29@gmail.com>
2022-06-17 18:53:30 +0700

    fix : perbaikan code [repository\entity.go]

--- backend/repository/entity.go
+++ backend/repository/entity.go
@@ -1,6 +1,15 @@
 package repository
 
 
+type User struct {
+	ID       int64  `db:"id"`
+	Username string `db:"username"`
+	Password string `db:"password"`
+	Role     string `db:"role"`
+	Loggedin bool   `db:"loggedin"`
+	Token    string `db:"token"`
+}
+
 type admin struct {
 	ID       int64  `db:"id"`
 	Username string `db:"username"`
@@ -29,8 +38,7 @@ type peminjaman struct {
 	tanggal_kembali string `db:"tanggalKembali"`
 }
 
-type denda struct {
-	ID       int64  `db:"id"`
+
 	
 
 
