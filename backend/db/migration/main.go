9ae21713f08ba1486259f1d1583aa815919bb785
ilmiDs <ilmiibokcom@gmail.com>
2022-06-17 14:51:39 +0800

    chore : revisi database

---
+++ backend/db/migration/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Run This Script for migration db
+func main() {
+	db, err := sql.Open("sqlite3", "backend/bd/migrations.db")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS users (
+    id_user integer not null primary key AUTOINCREMENT,
+    username varchar(255) not null,
+    password varchar(255) not null,
+    role varchar(255) not null,
+    loggedin boolean not null
+);
+
+
+	CREATE TABLE IF NOT EXISTS admin (
+	id_admin integer not null primary key AUTOINCREMENT,
+	username varchar(255) not null,
+	password varchar(255) not null,
+	role varchar(255) not null,
+	loggedin boolean not null
+);
+
+	CREATE TABLE IF NOT EXISTS buku (
+	id_buku integer not null primary key AUTOINCREMENT,
+	ISBN integer not null primary key AUTOINCREMENT,
+	judul varchar(255) not null,
+	pengarang varchar(255) not null,
+	penerbit varchar(255) not null,
+	tahun_terbit integer not null,
+	jumlah_buku integer not null,
+	kategori varchar(255) not null
+	description varchar(255) not null
+);
+
+	CREATE TABLE IF NOT EXISTS peminjaman (
+	id_peminjaman integer not null primary key AUTOINCREMENT,
+	tanggal_pinjam date not null,
+	tanggal_kembali date not null,
+	status boolean not null
+	foreign key(id_buku) references buku(id_buku)
+	foreign key(id_user) references users(id_user)
+);
+
+	CREATE TABLE IF NOT EXISTS pengembalian (
+	id_pengembalian integer not null primary key AUTOINCREMENT,
+	tanggal_kembali date not null,
+	status boolean not null
+	foreign key(id_buku) references buku(id_buku)
+	foreign key(id_user) references users(id_user)
+	);
+
+	CREATE TABLE IF NOT EXISTS denda (
+	id_denda integer not null primary key AUTOINCREMENT,
+	tanggal_denda date not null,
+	jumlah_denda integer not null,
+	foreign key(id_peminjaman) references peminjaman(id_peminjaman)
+	foreign key(id_user) references users(id_user)
+);
+
+
+
+	) 
+
+
+
+INSERT INTO users(username, password, role, loggedin) VALUES
+    ('aditira', '1234', 'admin', false),
+    ('dina', '4321', 'employee', false),
+    ('dito', '2552', 'employee', false),
+	('aris', '2910', 'admin', false);
+
+`)
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ backend/repository/users.go
@@ -0,0 +1,115 @@
+package repository
+
+import (
+	"database/sql"
+	"errors"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (u *UserRepository) FetchUserByID(id int64) (User, error) {
+	var user User
+	sqlStatement  := `SELECT id, username, password, role, loggedin FROM users
+	WHERE id = ?`
+
+	row := u.db.QueryRow(sqlStatement)
+	err := row.Scan(
+		&user.ID,
+		&user.Username,
+		&user.Password,
+		&user.Role,
+		&user.Loggedin,
+	)
+
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil // TODO: replace this
+}
+
+func (u *UserRepository) FetchUsers() ([]User, error) {
+	var sqlStatement string
+	var users []User
+
+	//TODO: add sql statement here
+	//HINT: join table cart_items and products
+	sqlStatement = `SELECT id, username, password, role, loggedin FROM users`
+
+	rows, err := u.db.Query(sqlStatement)
+	if err != nil {
+		return nil , err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var user User
+
+		err := rows.Scan(
+			&user.ID,
+			&user.Username,
+			&user.Password,
+			&user.Role,
+			&user.Loggedin,
+		)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil // TODO: replace this
+}
+
+func (u *UserRepository) Login(username string, password string) (*string, error) {
+
+	users, err := u.FetchUsers()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, user := range users {
+		if user.Username == username {
+			if user.Password == password {
+				return &user.Username, nil
+			} else {
+				return nil, errors.New("Login Failed")
+			}
+		}
+	}
+	return nil, errors.New("Login Failed") // TODO: replace this
+}
+
+func (u *UserRepository) InsertUser(username string, password string, role string, loggedin bool) error {
+	sqlStatement := `INSERT INTO users (username, password, role, loggedin) 
+	VALUES (?, ?, ?, ?)`
+
+	_, err := u.db.Exec(sqlStatement, username, password, role, loggedin)
+	if err != nil {
+		return err
+	}
+	return nil // TODO: replace this
+}
+
+func (u *UserRepository) FetchUserRole(username string) (*string, error) {
+	sqlStatement := `SELECT role FROM users WHERE username = ?`
+
+	row := u.db.QueryRow(sqlStatement, username)
+	var role string 
+
+	err := row.Scan(
+		&role,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &role, nil // TODO: replace this
+}
