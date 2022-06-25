a003d23bdd4272bd30bf7d6dcd11f9ed87a9f4c5
ilmiDs <ilmiibokcom@gmail.com>
2022-06-25 15:06:08 +0800

    db

--- backend/db/migration/main.go
+++ backend/db/migration/main.go
@@ -8,71 +8,76 @@ import (
 
 // Run This Script for migration db
 func main() {
-	db, err := sql.Open("sqlite3", "backend/db/migrations.db")
+	db, err := sql.Open("sqlite3", "db/perpus.db")
 	if err != nil {
 		panic(err)
 	}
 
 	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS users (
-    id_user integer not null primary key AUTOINCREMENT,
-    username varchar(255) not null,
-    password varchar(255) not null,
-    role varchar(255) not null,
-    loggedin boolean not null
-);
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			nim INTEGER ,
+			nama varchar(255) NOT NULL,
+			gender varchar(255) ,
+			email varchar(255) NOT NULL,
+			jurusan varchar(255) ,
+			fakultas varchar(255) ,
+			noHp varchar(255),
+			password varchar(255) NOT NULL,
+			role varchar(255) NOT NULL,
+			logged bool NOT NULL,
+			gambar varchar(255)
+		);
 
+		CREATE TABLE IF NOT EXISTS buku (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			ISBN INTEGER NOT NULL,
+			judul varchar(255) NOT NULL,
+			pengarang varchar(255) NOT NULL,
+			penerbit varchar(255) NOT NULL,
+			tahun varchar(255) NOT NULL,
+			stok INTEGER NOT NULL,
+			kotaTerbit varchar(255) NOT NULL,
+			deskripsi varchar(255) NOT NULL,
+			gambar varchar(255) NOT NULL
+		);
 
+		CREATE TABLE IF NOT EXISTS peminjaman (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			idUser INTEGER NOT NULL,
+			idBuku INTEGER NOT NULL,
+			tanggalPeminjaman DATE NOT NULL,
+			tanggalPengembalian DATE NOT NULL,
+			status varchar(255) NOT NULL,
+			foreign key(idUser) references users(id),
+			foreign key(idBuku) references buku(id)
+		);
 
-	CREATE TABLE IF NOT EXISTS buku (
-	id_buku integer not null primary key AUTOINCREMENT,
-	ISBN integer not null,
-	judul varchar(255) not null,
-	pengarang varchar(255) not null,
-	penerbit varchar(255) not null,
-	tahun_terbit integer not null,
-	jumlah_buku integer not null,
-	kategori varchar(255) not null,
-	description varchar(255) not null
-);
+		CREATE TABLE IF NOT EXISTS pengembalian (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			idUser INTEGER NOT NULL,
+			idBuku INTEGER NOT NULL,
+			tanggalPengembalian DATE NOT NULL,
+			status varchar(255) NOT NULL,
+			foreign key(idUser) references users(id),
+			foreign key(idBuku) references buku(id)
+		);
 
-	CREATE TABLE IF NOT EXISTS peminjaman (
-	id_peminjaman integer not null primary key AUTOINCREMENT,
-	id_buku integer not null,
-	id_user integer not null,
-	tanggal_pinjam date not null,
-	tanggal_kembali date not null,
-	status boolean not null,
-	foreign key(id_buku) references buku(id_buku),
-	foreign key(id_user) references users(id_user)
-);
+		CREATE TABLE IF NOT EXISTS pinjamBuku (
+			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			nama varchar(255) NOT NULL,
+			nim INTEGER NOT NULL,
+			judul varchar(255) NOT NULL,
+			pengarang varchar(255) NOT NULL,
+			noBuku INTEGER NOT NULL,
+			foreign key(nama) references users(nama),
+			foreign key(nim) references users(nim)
+			foreign key(noBuku) references buku(id)
 
-	CREATE TABLE IF NOT EXISTS pengembalian (
-	id_pengembalian integer not null primary key AUTOINCREMENT,
-	tanggal_kembali date not null,
-	id_buku integer not null,
-	id_user integer not null,
-	status boolean not null,
-	foreign key(id_buku) references buku(id_buku),
-	foreign key(id_user) references users(id_user)
-);
+		);
 
-	CREATE TABLE IF NOT EXISTS denda (
-	id_denda integer not null primary key AUTOINCREMENT,
-	id_peminjaman integer not null,
-	id_user integer not null,
-	tanggal_denda date not null,
-	jumlah_denda integer not null,
-	foreign key(id_peminjaman) references peminjaman(id_peminjaman),
-	foreign key(id_user) references users(id_user)
-);
-
-
-INSERT INTO users(username, password, role, loggedin) VALUES
-    ('aditira', '1234', 'admin', false),
-    ('dina', '4321', 'employee', false),
-    ('dito', '2552', 'employee', false),
-	('aris', '2910', 'admin', false);
+		INSERT INTO users (nama, email, password, role, logged) VALUES 
+		("admin", "backend@gmail.com", "admin", "admin", false);	
 
 `);
 
